7c3b014b48fe5f1f8cbb56ef42330d4f64d726c5
MikaJS94 <pawel.mika94@gmail.com>
2022-10-18 14:40:01 +0200

    git

---
+++ index.js
@@ -0,0 +1,7 @@
+const dzialanie = document.querySelector(".aktualne_dzialanie");
+const liczba = document.querySelectorAll(".liczba");
+
+var wpise = "";
+
+liczba.forEach((element)=> {element.addEventListener("click",()=> console.log(element))});
+
