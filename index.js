7a3b15ef493707d13c572f8c80a4bdc30bf9c353
MikaJS94 <pawel.mika94@gmail.com>
2022-10-18 22:52:25 +0200

    nic

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const dzialanie = document.querySelector(".aktualne_dzialanie");
 const liczba = document.querySelectorAll(".liczba");
 
 var wpis = "";
+let a = "";
 
 liczba.forEach((element)=> {element.addEventListener("click",()=> 
 {wpis = element.innerText;
-dzialanie.innerText = wpis;
-console.log(wpis);
+   a = a.toString() + wpis.toString() ;
+dzialanie.innerText = a;
+console.log(a);
 
 }
     
