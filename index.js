4d65c375391c6946f2befe613151dff2c6cc83c2
MikaJS94 <pawel.mika94@gmail.com>
2022-10-23 21:53:27 +0200

    aktualny kalkulator

--- index.js
+++ index.js
@@ -1,65 +1,123 @@
-const dzialanie = document.querySelector(".aktualne_dzialanie");
+const aktualne_dzialanie = document.querySelector(".aktualne_dzialanie");
 const liczba = document.querySelectorAll(".liczba");
 const operatory = document.querySelectorAll(".operator");
 const usun = document.querySelector(".usun");
-const poprzednie = document.querySelector(".poprzednie_dzialanie");
-var wpis = "";
-let operator = "";
-let a = "";
-let b = "";
+const poprzednie_dzialanie = document.querySelector(".poprzednie_dzialanie");
+let liczby = "";
+let operator = undefined;
+let akt_liczba = "";
+let pop_liczba = "";
+let pop;
+let akt;
+let akt_operator;
 
 
-function wypisz (wpisz)
-{
+// function wypisz (wpisz)
+// {
    
-   if(wpisz == ".")
-   {
-   if(!(a.includes(wpisz)))
-   {
-      a = a.toString() + wpisz.toString() ;
-   }
-   }
-   else a = a.toString() + wpisz.toString() ;
+//    if(wpisz == ".")
+//    {
+//    if(!(a.includes(wpisz)))
+//    {
+//       a = a.toString() + wpisz.toString() ;
+//    }
+//    }
+//    else a = a.toString() + wpisz.toString() ;
    
-   dzialanie.innerText = a;
+//    dzialanie.innerText = a;
    
    
-   console.log(a);
-   console.log(b);
+//    console.log(a);
+//    console.log(b);
    
  
-}
+// }
 
-function popdzial (wpisz)
-   {
-      b = b.toString()+a;
-      a="";
-      b = b.toString() + wpisz.toString() ;
+// function popdzial (wpisz)
+//    {
+//       b = b.toString()+a;
+//       a="";
+//       b = b.toString() + wpisz.toString() ;
       
-      poprzednie.innerText = b;
+//       poprzednie.innerText = b;
       
-      dzialanie.innerText = "";
+//       dzialanie.innerText = "";
       
-   }
+//    }
 
-usun.addEventListener("click",()=> {dzialanie.innerText = ""; a="";poprzednie.innerText = ""});
 
 liczba.forEach((element)=> {element.addEventListener("click",()=> 
-{wpis = element.innerText;
-   wypisz (wpis);
+{
+   liczby = element.innerText;
+   akt_liczba = akt_liczba + liczby;
+   aktualne_dzialanie.innerText = akt_liczba;
+   
+   
 }
 )});
 
+function oblicz()
+{
+   
+   pop = parseFloat(pop_liczba);
+   akt = parseFloat(akt_liczba);
+   console.log(operator);
+   console.log(pop);
+   console.log(akt);
+   if(isNaN(pop) || isNaN(akt)) {
+      return
+    }
+    
+   switch(operator)
+   {
+      case "+":
+         pop = pop +akt;   
+         break;
+      case "-":
+         pop = pop -akt;   
+         break;
+      case "/":
+         pop = pop /akt;   
+         break;
+      case '×':
+         pop = pop *akt;   
+         break;
+   }
+   akt_liczba = pop.toString();
+   
+   console.log("dupa");
+   
+
+}
+
+//usun.addEventListener("click",()=> {dzialanie.innerText = ""; a="";poprzednie.innerText = ""});
+
+function aktulizuj(operator)
+{
+   poprzednie_dzialanie.innerText = akt_liczba.toString()+ operator;
+   
+
+
+      pop_liczba = akt_liczba.toString();
+      akt_liczba = "";
+      aktualne_dzialanie.innerText = "";
+      akt_liczba = "";
+}
+function wybierzOperacje (operacja)
+{
+   oblicz();
+   operator = operacja;
+}
+ 
+
 operatory.forEach((element)=>{
    element.addEventListener("click",()=>
-   {
+   {  
       
       
-      operator = element.innerText;
-      popdzial(operator);
-      dzialanie.innerText = "";
+      wybierzOperacje(element.innerText);  
+      aktulizuj(element.innerText);   
       
    }
-   )
-});
-
+)});
+   
